0d2b6b1f508108705345804d42f24af49de8c960
Jack Hickey <jack.hickey@outlook.com>
2021-07-25 11:45:10 +0100

    -Created Windows subdir -Created WindowMain window

--- Teams.Presence.Setter/Windows/WindowMain.xaml.cs
+++ Teams.Presence.Setter/Windows/WindowMain.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Teams.Presence.Setter
+namespace Teams.Presence.Setter.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WindowMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowMain : Window
     {
-        public MainWindow()
+        public WindowMain()
         {
             InitializeComponent();
         }
